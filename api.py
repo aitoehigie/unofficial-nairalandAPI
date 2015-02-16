2b2586104741e6ef1aad0b58afa80a0432782629
aitoehigie <aitoehigie@gmail.com>
2015-02-16 06:17:44 +0100

    revert back to functional style

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ Revised: June 2014
 
 import requests
 
-nairaland_boards = dict(
+BOARDS = dict(
                         Technology=8, Programming=34,
                         software_programmer_market=76,
                         Webmasters=30, web_market=52, Computers=22,
@@ -45,42 +45,16 @@ nairaland_boards = dict(
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36'
 
+NAME = "nairaland username"
+PASSWORD = "nairaland password"
+ROOT_URL = "http://www.nairaland.com/"
 
-class Nairaland(object):
-    """This creates a nairaland object which represents a user"""
 
-    def __init__(self, name, password):
-        self.name = name
-        self.password = password
-        self.login_params = dict(name=self.name, password=self.password)
-    #self.login()
+def login(name=NAME, password=PASSWORD)
+    #Nairaland login function
+    login_params = dict(name = NAME, password = PASSWORD)
+    session = requests.Session()
+    user = session.post(ROOT_URL+"do_login", data=login_params)
+    return user
 
-    def login(self):
-        self.user = requests.Session()
-        self.user.headers.update({"User-Agent":
-        	                       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2)\
-                                   AppleWebKit/537.36 (KHTML, like Gecko) \
-                                   Chrome/36.0.1944.0 Safari/537.36"})
-        self.user.post("http://www.nairaland.com/do_login",
-                        data=self.login_params)
-        return self.user
 
-    def post_topic(self, title, body, board):
-        self.user.post("http://www.nairaland.com/do_newtopic",
-                       data=dict(title=title, body=body, board=board,
-                       session=self.user.cookies["session"]))
-
-    def logout(self):
-        self.user.post("http://www.nairaland.com/do_logout",
-                       data=self.user.cookies["session"])
-
-    # def change_email(self, email):
-    # """This has been disabled on nairaland"""
-    # self.user.post("http://www.nairaland.com/do_changeemail",
-    # data=dict(email=email, session=self.user.cookies["session"]))
-
-    # def change_password(self, oldpassword, password, password2):
-    # """This has been disabled on nairaland"""
-    # self.user.post("http://www.nairaland.com/do_changepass",
-    # data=dict(oldpassword=oldpassword, password=password,
-    # password2=password2, session=self.user.cookies["session"]))
